02efb854856aece0d9fc61bb7be3ffc43bc8baae
BradlyBarnacles <calum@fuzetechservices.co.uk>
2022-06-27 23:59:43 +0100

    create component

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
 	export let name: string;
+	import Card from "./components/Card.svelte"
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<Card></Card>
+	<Card></Card>
+	<Card></Card>
 </main>
 
 <style>
---
+++ src/components/Card.svelte
@@ -0,0 +1,12 @@
+
+
+<div></div>
+
+<style>
+    div {
+        height: 100px;
+        width: 100px;
+        background-color: rebeccapurple;
+        border-radius: 10px;
+    }
+</style>
